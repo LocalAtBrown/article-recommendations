4b70590864efcf4ec0043fce5390aaa6cb0519e2
Erick Martinez <44077419+E-Rick@users.noreply.github.com>
2021-01-14 16:39:04 -0500

    feat: add JS script, register and load it in WP Admin

--- wp-js-plugin-start.php
+++ wp-js-plugin-start.php
@@ -9,3 +9,48 @@
  *
  * @package wp-js-plugin-starter
  */
+
+/**
+ * Retrieves a URL to a file in the plugin's directory.
+ *
+ * @param  string $path Relative path of the desired file.
+ *
+ * @return string       Fully qualified URL pointing to the desired file.
+ *
+ * @since 1.0.0
+ */
+function wp_js_plugin_starter_url( $path ) {
+	return plugins_url( $path, __FILE__ );
+}
+
+/**
+ * Registers the plugin's script to be enqueued later.
+ *
+ * @since 1.0.0
+ */
+function wp_js_plugin_starter_register_script() {
+	wp_register_script(
+		'wp-js-plugin-starter',
+		wp_js_plugin_starter_url( 'src/index.js' ),
+		array()
+  );
+}
+
+/**
+ * Enqueues the plugin's script. (loads it into Wordpress)
+ *
+ * @since 1.0.0
+ */
+function wp_js_plugin_starter_enqueue_script() {
+	wp_enqueue_script( 'wp-js-plugin-starter' );
+}
+
+/**
+ * Trigger the script registration on init.
+ */
+add_action( 'init', 'wp_js_plugin_starter_register_script' );
+
+/**
+ * Enqueue the script in all admin pages
+ */
+add_action( 'admin_enqueue_scripts', 'wp_js_plugin_starter_enqueue_script' ); 
