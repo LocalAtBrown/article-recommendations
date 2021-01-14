4b8eb56834f2d45bf85a1e4af5fdb3a205e30cb4
Erick Martinez <44077419+E-Rick@users.noreply.github.com>
2021-01-14 15:45:53 -0500

    feat: create an empty WordPress plugin

---
+++ wp-js-plugin-start.php
@@ -0,0 +1,11 @@
+<!-- This file contains metadata about the WP plugin written as a PHP comment  -->
+<?php
+/**
+ * Plugin Name: WP JS Plugin Starter
+ * Plugin URI: https://github.com/youknowriad/wp-js-plugin-starter
+ * Description: Just another WordPress plugin starter
+ * Version: 1.0.0
+ * Author: Erick Martinez Jr
+ *
+ * @package wp-js-plugin-starter
+ */
