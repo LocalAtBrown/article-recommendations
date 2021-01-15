c1e3396c17e3d3807990ff0bbf266fb5c740b243
Erick Martinez <44077419+E-Rick@users.noreply.github.com>
2021-01-15 12:00:34 -0500

    feat: add gutenberg block

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 
 const { createElement } = wp.element
+const { registerBlockType } = wp.blocks
+import { __ } from '@wordpress/i18n'; 
 
-console.log(<div>Start the JSX engine!</div>)
+registerBlockType("wp-js-plugin-starter/hello-world", {
+  title: "Hello World",
+  description: "Just another Hello World block",
+  icon: "admin-site",
+  category: "common",
+
+  edit: function() {
+    return <p>Hello Editor</p>;
+  },
+
+  save: function() {
+    return <p>Hello Frontend</p>;
+  }
+});
