a56bc24c21e9f1b571f0fcfe31685562c6906f1a
Erick Martinez <44077419+E-Rick@users.noreply.github.com>
2021-01-18 17:52:24 -0500

    docs: add comments

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ const { registerBlockType } = wp.blocks
 import { __ } from '@wordpress/i18n'; 
 
 registerBlockType("article-recommendations/hello-world", {
-  title: "Hello World",
-  description: "Just another Hello World block",
-  icon: "admin-site",
-  category: "common",
-
+  // built-in attributes
+  title: "Article Recommendations",
+  description: "Create a list of article recommendations",
+  // can be wp icons by dash icons from wp or put svg
+  icon: "admin-site", 
+  // Category the gutenberg block type falls under within the editor
+  category: "widgets", 
+  // custom attributes
+  attributes: {},
+  // custom functions
+  // built-in functions
+  // uses JSX syntax
   edit: function() {
     return <p>Hello Editor Recommendations</p>;
   },
