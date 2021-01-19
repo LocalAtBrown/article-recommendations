9fddb85b310cd3c2b2bcac1061c882bb3b2c104b
Erick Martinez <44077419+E-Rick@users.noreply.github.com>
2021-01-18 19:51:18 -0500

    chore: add WP prettier, linter, and config

--- src/index.js
+++ src/index.js
@@ -1,26 +1,23 @@
+const { registerBlockType } = wp.blocks;
 
-const { createElement } = wp.element
-const { registerBlockType } = wp.blocks
-import { __ } from '@wordpress/i18n'; 
+registerBlockType( 'article-recommendations/hello-world', {
+	// built-in attributes
+	title: 'Article Recommendations',
+	description: 'Create a list of article recommendations',
+	// can be wp icons by dash icons from wp or put svg
+	icon: 'admin-site',
+	// Category the gutenberg block type falls under within the editor
+	category: 'widgets',
+	// custom attributes
+	attributes: {},
+	// custom functions
+	// built-in functions
+	// uses JSX syntax
+	edit() {
+		return <p>Hello Editor Recommendations</p>;
+	},
 
-registerBlockType("article-recommendations/hello-world", {
-  // built-in attributes
-  title: "Article Recommendations",
-  description: "Create a list of article recommendations",
-  // can be wp icons by dash icons from wp or put svg
-  icon: "admin-site", 
-  // Category the gutenberg block type falls under within the editor
-  category: "widgets", 
-  // custom attributes
-  attributes: {},
-  // custom functions
-  // built-in functions
-  // uses JSX syntax
-  edit: function() {
-    return <p>Hello Editor Recommendations</p>;
-  },
-
-  save: function() {
-    return <p>Hello Frontend</p>;
-  }
-});
+	save() {
+		return <p>Hello Frontend</p>;
+	},
+} );
