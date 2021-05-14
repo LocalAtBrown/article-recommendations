f004589154205a84e086ec0dc2206893822d5d7d
Erick Martinez <44077419+E-Rick@users.noreply.github.com>
2021-05-14 16:38:15 -0400

    feat: add activator and deactivator plugin classes

---
+++ includes/class-article-recommendations-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/LocalAtBrown/article-recommendations
+ * @since      1.0.0
+ *
+ * @package    Article_Recommendations
+ * @subpackage Article_Recommendations/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Article_Recommendations
+ * @subpackage Article_Recommendations/includes
+ * @author     Erick Martinez Jr <em3591@columbia.edu>
+ */
+class Article_Recommendations_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-article-recommendations-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/LocalAtBrown/article-recommendations
+ * @since      1.0.0
+ *
+ * @package    Article_Recommendations
+ * @subpackage Article_Recommendations/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Article_Recommendations
+ * @subpackage Article_Recommendations/includes
+ * @author     Erick Martinez Jr <em3591@columbia.edu>
+ */
+class Article_Recommendations_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
