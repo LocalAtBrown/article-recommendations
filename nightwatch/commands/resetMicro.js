ed1653113fb34db4afa9caa0858d07aa4cd6f4bb
Erick Martinez <44077419+E-Rick@users.noreply.github.com>
2021-07-19 12:02:20 -0400

    test(nightwatch): add resetMicro command

---
+++ nightwatch/commands/resetMicro.js
@@ -0,0 +1,31 @@
+/**
+Clears Snowplow Micro's cache - to be used before each test
+**/
+const EventEmitter = require('events');
+
+class ResetMicro extends EventEmitter {
+	async command() {
+		const request = require('request');
+		// const localtunnel = require("localtunnel");
+		// const tunnel = await localtunnel({port: 9090});
+
+		// const options = {
+		// 	url: `${tunnel.url}/micro/reset`,
+		// 	headers: {
+		// 		"User-Agent": "request",
+		// 		"Bypass-Tunnel-Reminder": "true",
+		// 	},
+		// };
+		// console.log(options.url);
+		request('http://localhost:9090/micro/reset', (err, res, body) => {
+			if (err) {
+				console.log(err);
+				throw 'Unable to reset micro';
+			}
+		});
+		this.emit('complete');
+		return this;
+	}
+}
+
+module.exports = ResetMicro;
